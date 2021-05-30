f7ebc0db8d48b88c8ea2810d77a415dbdf0c9da6
Abner <abnercabrera28@gmail.com>
2021-05-30 14:43:36 -0400

    add inner HTML to form and append it to formContainer

--- src/index.js
+++ src/index.js
@@ -19,4 +19,6 @@ function fetchOrchestras() {
 function createForm() {
     const formContainer = document.getElementById("form-container")
     const form = document.createElement("form")
+    form.innerHTML = `<input id="name-input" placeholder="Name" type="text"/><br><input id="submit" value="Create Orchestra" type="submit"/>`
+    formContainer.append(form)
 }
