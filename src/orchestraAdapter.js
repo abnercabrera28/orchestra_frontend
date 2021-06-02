14f4d3fc48180c4030506345127732edc7ea8196
Abner <abnercabrera28@gmail.com>
2021-06-02 18:16:33 -0400

    add fetchOrchestras method

--- src/orchestraAdapter.js
+++ src/orchestraAdapter.js
@@ -4,4 +4,11 @@ class OrchestraAdapter {
         this.orchestraURL = `${orchestraURL}/orchestras`
     }
 
+    fetchOrchestras() {
+        fetch(this.orchestraURL)
+        .then(r => r.json())
+        .then(orchestras => console.log(orchestras))
+        .catch(err => console.warn(err))
+    }
+
 }
