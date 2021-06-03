de220dd50ade8bdfb9f90542894b2d3303f2341a
Abner <abnercabrera28@gmail.com>
2021-06-03 15:27:50 -0400

    add editOrchestra method

--- src/orchestraAdapter.js
+++ src/orchestraAdapter.js
@@ -11,4 +11,29 @@ class OrchestraAdapter {
         .catch(err => console.warn(err))
     }
 
+    editOrchestra(){
+        fetch(`http://localhost:3000/orchestras/${editMode.dataset.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({
+                name: name.value
+            })
+        })
+        .then(r => r.json())
+        .then(info => {
+            if (info.status === 204) {
+                editMode.children[0].innerText = info.orchestra.name
+                editMode = false
+                document.getElementById("orchestra-submit").value = "Create Store"
+                name.value = ""
+            } else {
+                alert(info.errors)
+            }
+        })
+       .catch(err => console.error(err))
+    }
+
 }
