ac4c83f753f069a2c9f15b2cab9faffa67a7416a
Abner <abnercabrera28@gmail.com>
2021-06-03 17:40:41 -0400

    add all array to push Instrument objects in

--- src/instrument.js
+++ src/instrument.js
@@ -1,9 +1,14 @@
 class Instrument {
+    
+    static all = []
+    
     constructor({id, name, family, quantity, orchestra_id}) {
         this.id = id
         this.name = name
         this.family = family
         this.quantity = quantity
         this.orchestra_id = orchestra_id
+
+        Instrument.all.push(this)
     }
 }
