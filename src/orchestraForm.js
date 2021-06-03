5289129a098d07de391298c13cc2fe2db0993386
Abner <abnercabrera28@gmail.com>
2021-06-03 16:20:20 -0400

    add createForm method

--- src/orchestraForm.js
+++ src/orchestraForm.js
@@ -1,3 +1,10 @@
 class OrchestraForm {
+    createForm() {
+        const formContainer = document.getElementById("form-container")
+        const form = document.createElement("form")
+        form.innerHTML = `<input id="name-input" placeholder="Name" type="text"/><br><input id="orchestra-submit" value="Create Orchestra" type="submit"/>`
+        formContainer.append(form)
     
+        form.addEventListener("submit", handleSubmit)
+    }
 }
