9932fa5c66b0dbcfbbab2295e6dfaf53a51bcb8f
Abner <abnercabrera28@gmail.com>
2021-06-03 17:38:06 -0400

    add constructor for Instrument class

--- src/instrument.js
+++ src/instrument.js
@@ -1,3 +1,9 @@
 class Instrument {
-    
+    constructor({id, name, family, quantity, orchestra_id}) {
+        this.id = id
+        this.name = name
+        this.family = family
+        this.quantity = quantity
+        this.orchestra_id = orchestra_id
+    }
 }
