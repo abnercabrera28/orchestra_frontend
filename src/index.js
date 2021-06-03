d9f9c9b63b7a5e0ac70124fe0734bad7722be152
Abner <abnercabrera28@gmail.com>
2021-06-03 12:12:40 -0400

    change input id in createForm function to orchestra-submit

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function fetchOrchestras() {
 function createForm() {
     const formContainer = document.getElementById("form-container")
     const form = document.createElement("form")
-    form.innerHTML = `<input id="name-input" placeholder="Name" type="text"/><br><input id="submit" value="Create Orchestra" type="submit"/>`
+    form.innerHTML = `<input id="name-input" placeholder="Name" type="text"/><br><input id="orchestra-submit" value="Create Orchestra" type="submit"/>`
     formContainer.append(form)
 
     form.addEventListener("submit", handleSubmit)
