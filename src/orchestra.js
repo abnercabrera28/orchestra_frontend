bfcc318cb6fe1142bd74f8e163f0517ce726a53e
Abner <abnercabrera28@gmail.com>
2021-06-03 15:56:20 -0400

    add static all array to pushthis

--- src/orchestra.js
+++ src/orchestra.js
@@ -1,8 +1,13 @@
 class Orchestra {
 
+    static all = []
+
     constructor({id, name, instruments}) {
         this.id = id
         this.name = name
         this.instruments = instruments
+       
+        Orchestra.all.push(this)
     }
+
 }
